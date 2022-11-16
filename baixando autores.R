06e30dffd8b4eb78023a7521b13498408d6aa636
Beatriz Milz <milz.bea@gmail.com>
2022-11-16 18:19:06 -0300

    base tweets mais pops

--- baixando autores.R
+++ baixando autores.R
@@ -24,6 +24,7 @@ base_bruta_filtrada <- base_bruta |>
   dplyr::distinct(id, .keep_all = TRUE)
 
 
+# buscar autores ------
 autores_buscar <- base_bruta_filtrada |> 
   dplyr::distinct(author_id) |> 
   dplyr::pull(author_id)
@@ -31,9 +32,31 @@ autores_buscar <- base_bruta_filtrada |>
 
 autores_tweets <- academictwitteR::get_user_profile(autores_buscar)
 
-autores_tweets |> 
+autores_tweets_arrumado <- autores_tweets |> 
   tibble::as_tibble() |> 
   janitor::clean_names() |> 
-  dplyr::arrange(desc(public_metrics$followers_count)) |> 
+  dplyr::arrange(desc(public_metrics$followers_count)) 
+
+autores_tweets_arrumado |> 
   readr::write_rds("data/autores_tweets.rds")
-  
+
+
+# unir bases -----
+library(readr)
+
+autores <- readr::read_rds("data/autores_tweets.rds") |> 
+  tidyr::unnest(cols = c(public_metrics)) |> 
+  janitor::clean_names() |> 
+  dplyr::select(username:url, location) |> 
+  dplyr::mutate(
+    dplyr::across(.names = "autor_{.col}")) |> 
+  dplyr::select(tidyselect::starts_with("autor_"))
+
+
+base_tweets_mais_pops <- dplyr::left_join(base_bruta_filtrada, metricas_tweets_filtrados,
+                 by = c("id")) |>
+  dplyr::left_join(autores, by = c("author_id" = "autor_id")) |>
+  dplyr::arrange(desc(like_count))
+
+
+base_tweets_mais_pops |> write_rds("data/base_tweets_mais_pops.rds")  
