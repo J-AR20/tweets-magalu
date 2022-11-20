693a7427b96b55a27dc02025b335fb6bc6622e3b
Beatriz Milz <milz.bea@gmail.com>
2022-11-20 10:43:30 -0300

    salvando a base final

--- 01-baixar-tweets.R
+++ 01-baixar-tweets.R
@@ -1,2 +1,18 @@
 # código com Jonas :)
 # colocar aqui depois.
+
+
+
+# Salvar a base final - tirar repeticoes
+library(readr)
+
+base_bruta <- read_rds("data/base_bruta.rds")
+
+base_bruta_distinta <- base_bruta |> 
+  dplyr::select(
+    -c(referenced_tweets, edit_history_tweet_ids,
+       entities, public_metrics, geo, attachments, withheld)
+  ) |> 
+  dplyr::distinct(id, .keep_all = TRUE) 
+
+write_rds(base_bruta_distinta, "data/base_bruta_final.rds")
