9054c5d87ddf8e0361084b835ca2130eedc96685
Beatriz Milz <milz.bea@gmail.com>
2022-11-20 15:38:17 -0300

    salva a base completa

--- 04-unir-bases.R
+++ 04-unir-bases.R
@@ -13,9 +13,8 @@ base_users <- readr::read_rds("data/base_user_final.rds")
 base_tweets_completa <- dplyr::left_join(base_bruta, base_metricas,
                                           by = c("id")) |>
   dplyr::left_join(base_users, by = c("author_id" = "author_id")) |>
-  dplyr::arrange(desc(like_count))
-
-# TODO: REMOVER COLUNAS DESNECESSÁRIAS
-
+  dplyr::arrange(desc(like_count)) |> 
+  dplyr::select(text = text.x, author_id:id, query, data_pesquisa, 
+                retweet_count:quote_count,author_name:author_created_at)
 
 base_tweets_completa |> write_rds("data/base_tweets_completa.rds")  
