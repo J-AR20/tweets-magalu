25ffd70799425a95c90e46b3a2014891bc7eb444
Jonas Araujo <85358869+J-AR20@users.noreply.github.com>
2022-11-22 11:04:52 -0300

    Create 06-explorando_dados.R

---
+++ 06-explorando_dados.R
@@ -0,0 +1,28 @@
+library(academictwitteR)
+library(readr)
+library(writexl)
+library(purrr)
+library(readxl)
+library(dplyr)
+library(janitor)
+library(ggplot2)
+# install.packages('esquisse')
+library(esquisse)
+
+# lendo a base completa
+base_completa <- read_rds('data/data-bia/base_tweets_completa.rds') %>% 
+ filter(!author_username %in% c('magalu', 'luizatrajano')) # remover tweets magalu e luizatrajano 
+
+# preparação da base
+tweets_por_data <- base_completa %>% 
+  mutate(data = as.Date(created_at)) %>%  # ajustando a data
+  count(data) # contando quantas linhas existe para cada data
+
+# fazendo o gráfico de data através do pacote esquisse
+ggplot(tweets_por_data) +
+  aes(x = data, y = n) +
+  geom_line(size = 0.5, colour = "#112446") +
+  labs(
+    caption = "Dados extraídos da API do Twitter usando o pacote academictwitteR"
+  ) +
+  ggthemes::theme_fivethirtyeight()
