6625d5aa5677d7abcc81c988fd9eed497e4ce3dd
Jonas Araujo <85358869+J-AR20@users.noreply.github.com>
2022-11-29 10:15:06 -0300

    Update 07-explorando_categorias.r

--- 07-explorando_categorias.r
+++ 07-explorando_categorias.r
@@ -11,25 +11,21 @@ library(stringr)
 library(abjutils)
 
 # lendo a base completa
-base_completa <- read_rds('data/base_tweets_completa.rds') %>% 
+base_completa <- read_rds('data/data-bia/base_tweets_completa.rds') %>% 
   filter(!author_username %in% c('magalu', 'luizatrajano', 'HaddadDebochado', 'direitasiqueira', 'LUIZPATRIOTA39')) 
 # removi acima os tweets obviamente partidários da magalu e luizatrajano, e aqueles de perfis de humor
 
 url_google_sheet <- 'https://docs.google.com/spreadsheets/d/1z3oNAxrIYHZkjwVirRxLKiGMQAyxGE8cAemfXvusUUY/edit#gid=462148650'
 
-base_categoriza <- read_sheet(url_google_sheet, 'tweets-mais-de-cem-likes')
+base_categorizada <- read_sheet(url_google_sheet, 'tweets-mais-de-cem-likes')
 nome_categoria <- read_sheet(url_google_sheet, 'definicao-categoria') %>% 
   clean_names()
 
-base_categorizada_completa <- left_join(base_categoriza, base_completa, by = 'id') %>% 
+base_categorizada_completa <- left_join(base_categorizada, base_completa, by = 'id') %>% 
   left_join(nome_categoria, by = "cod_tweets_magalu") %>% 
   filter(!autor_username %in% c('magalu', 'luizatrajano', 'HaddadDebochado', 'direitasiqueira', 'LUIZPATRIOTA39'), 
          orientacao_categoria != '-') %>% 
   mutate(cod_tweets_magalu = as.character(cod_tweets_magalu)) 
-
-
-readr::write_rds(base_categorizada_completa, 'data/base_categorizada_completa.rds')
-
 
 sumarizacao_categorias <- base_categorizada_completa %>% 
   group_by(orientacao_categoria, cod_tweets_magalu, categoria) %>% 
@@ -51,12 +47,22 @@ extrem_words <- c('patriota', 'bolsonaro', 'armamentista', 'anti-esquerda',
                   'pro-vida','e conhecerao a verdade, e a verdade os libertara','conservador', '2️⃣2️⃣' ) %>% 
   paste0(collapse = '|')
 
-base_categorizada_completa %>%
-  filter(orientacao_categoria != 'outros') %>%
-  mutate(
-    author_description_clean = str_to_lower(author_description) %>% rm_accent(),
-    author_description_extrem = str_detect(author_description_clean, extrem_words),
-    author_name_clean = str_to_lower(author_name) %>% rm_accent(),
-    author_name_extrem = str_detect(author_name_clean, extrem_words)
-  )  %>%
+base_categorizada_completa %>% 
+  filter(orientacao_categoria != 'outros') %>% 
+  mutate(author_description_clean = str_to_lower(author_description) %>% rm_accent(),
+         author_description_extrem = str_detect(author_description_clean, extrem_words))
+
+base_categorizada_completa %>% 
+  filter(orientacao_categoria != 'outros') %>% 
+  mutate(author_name_clean = str_to_lower(author_name) %>% rm_accent(),
+         author_name_extrem = str_detect(author_name_clean, extrem_words)) %>% 
   View()
+
+# separando lista com os posicionamentos contrários da base_categorizada_completa para análise manual
+
+tweets_contrarios <- base_categorizada_completa %>% 
+  filter(orientacao_categoria != 'favoravel') %>% 
+  filter(orientacao_categoria != 'outros') %>% 
+  select(id, autor_username, author_description)
+
+write_xlsx(tweets_contrarios, 'data/tweets_contrarios.xlsx')
