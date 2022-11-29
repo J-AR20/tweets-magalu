c8f8fcfbc6aad75a2e31b80ec1801a0c514de66a
Beatriz Milz <milz.bea@gmail.com>
2022-11-29 15:46:51 -0300

    outra abordagem

--- 08-buscar-quem-segue.R
+++ 08-buscar-quem-segue.R
@@ -1,14 +1,21 @@
 library(readr)
 
 
-base_categorizada_completa <- read_rds("data/base_categorizada_completa.rds")
+# base_categorizada_completa <- read_rds("data/base_categorizada_completa.rds")
+# 
+# autores_unicos <- base_categorizada_completa |> 
+#   dplyr::arrange(desc(like_count)) |> 
+#   dplyr::distinct(author_id, author_username, .keep_all = TRUE)
 
-autores_unicos <- base_categorizada_completa |> 
-  dplyr::arrange(desc(like_count)) |> 
-  dplyr::distinct(author_id, author_username, .keep_all = TRUE)
+
+url_google_sheet <- 'https://docs.google.com/spreadsheets/d/1z3oNAxrIYHZkjwVirRxLKiGMQAyxGE8cAemfXvusUUY/edit#gid=462148650'
+
+base_users_contrarios <- googlesheets4::read_sheet(url_google_sheet, 'tweets_contrarios')
+
+lista_buscar <- base_users_contrarios |> dplyr::distinct(id, .keep_all = TRUE)
 
 
-autores_baixados <- list.files("data/author_following/") |> 
+users_baixados <- list.files("data/author_following/") |> 
   stringr::str_remove(".rds")
 
 
@@ -17,9 +24,8 @@ buscar_user_following <- function(id, username){
   readr::write_rds(df, paste0("data/author_following/", username, ".rds"))
 }
 
-autores_unicos |> 
-  dplyr::filter(!author_username %in% autores_baixados) |> 
-  dplyr::group_split(author_username) |> 
-  purrr::walk(~buscar_user_following(.x$author_id, .x$author_username))
+lista_buscar |> 
+  dplyr::filter(!autor_username %in% users_baixados) |> 
+  dplyr::group_split(id) |> 
+  purrr::walk(~buscar_user_following(id = .x$id, username =.x$autor_username))
   
-
