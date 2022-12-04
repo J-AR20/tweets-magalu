6b5a1a1b4ad6acaa6f524cbbbf549133ae9a8084
Beatriz Milz <milz.bea@gmail.com>
2022-12-04 19:38:28 -0300

    rodando os codigos novamente

--- 07-explorando_categorias.r
+++ 07-explorando_categorias.r
@@ -11,13 +11,14 @@ library(stringr)
 library(abjutils)
 
 # lendo a base completa
-base_completa <- read_rds('data/data-bia/base_tweets_completa.rds') %>% 
+base_completa <- read_rds('data/base_tweets_completa.rds') %>% 
   filter(!author_username %in% c('magalu', 'luizatrajano', 'HaddadDebochado', 'direitasiqueira', 'LUIZPATRIOTA39')) 
 # removi acima os tweets obviamente partidários da magalu e luizatrajano, e aqueles de perfis de humor
 
 url_google_sheet <- 'https://docs.google.com/spreadsheets/d/1z3oNAxrIYHZkjwVirRxLKiGMQAyxGE8cAemfXvusUUY/edit#gid=462148650'
 
 base_categorizada <- read_sheet(url_google_sheet, 'tweets-mais-de-cem-likes')
+
 nome_categoria <- read_sheet(url_google_sheet, 'definicao-categoria') %>% 
   clean_names()
 
@@ -30,34 +31,18 @@ base_categorizada_completa <- left_join(base_categorizada, base_completa, by = '
 sumarizacao_categorias <- base_categorizada_completa %>% 
   group_by(orientacao_categoria, cod_tweets_magalu, categoria) %>% 
   summarise(qtd_tweets = n(),
-            soma_like = sum(like_count),
-            soma_retweet = sum(retweet_count),
-            soma_respostas_tweet = sum(reply_count),
-            soma_quotes = sum(quote_count),
+            soma_like = sum(like_count, na.rm = TRUE),
+            soma_retweet = sum(retweet_count, na.rm = TRUE),
+            soma_respostas_tweet = sum(reply_count, na.rm = TRUE),
+            soma_quotes = sum(quote_count, na.rm = TRUE),
             usuarios = paste0(unique(autor_username), collapse = ', ')) %>% 
   arrange(desc(soma_like))
 
 write_xlsx(sumarizacao_categorias, 'data/sumaricao_categorias.xlsx')
 
-# DETECTANDO A EXTREMA-DIREITA --------------------------------------------
 
-extrem_words <- c('patriota', 'bolsonaro', 'armamentista', 'anti-esquerda', 
-                  'cristao de direita', 'cristao', 'deus, familia, patria','direita', 'anti comunas', 
-                  'desesquerdizando', 'liberdade', 'bolsonarista', 'brasil acima de tudo e deus acima de todos', 
-                  'pro-vida','e conhecerao a verdade, e a verdade os libertara','conservador', '2️⃣2️⃣' ) %>% 
-  paste0(collapse = '|')
 
-base_categorizada_completa %>% 
-  filter(orientacao_categoria != 'outros') %>% 
-  mutate(author_description_clean = str_to_lower(author_description) %>% rm_accent(),
-         author_description_extrem = str_detect(author_description_clean, extrem_words))
-
-base_categorizada_completa %>% 
-  filter(orientacao_categoria != 'outros') %>% 
-  mutate(author_name_clean = str_to_lower(author_name) %>% rm_accent(),
-         author_name_extrem = str_detect(author_name_clean, extrem_words)) %>% 
-  View()
-
+# Jonas explorando a base: 
 # separando a lista apenas com os posicionamentos contrários da base_categorizada_completa
 
 tweets_contrarios <- base_categorizada_completa %>% 
--- 10-analise-seguidores.R
+++ 10-analise-seguidores.R
@@ -1,3 +1,23 @@
+# DETECTANDO A EXTREMA-DIREITA --------------------------------------------
+
+base_categorizada_completa <- read_rds("data/base_categorizada_completa.rds")
+
+extrem_words <- c('patriota', 'bolsonaro', 'armamentista', 'anti-esquerda', 
+                  'cristao de direita', 'cristao', 'deus, familia, patria','direita', 'anti comunas', 
+                  'desesquerdizando', 'liberdade', 'bolsonarista', 'brasil acima de tudo e deus acima de todos', 
+                  'pro-vida','e conhecerao a verdade, e a verdade os libertara','conservador', '2️⃣2️⃣' ) %>% 
+  paste0(collapse = '|')
+
+base_categorizada_completa %>% 
+  filter(orientacao_categoria != 'outros') %>% 
+  mutate(author_description_clean = str_to_lower(author_description) %>% rm_accent(),
+         author_description_extrem = str_detect(author_description_clean, extrem_words),
+         author_name_clean = str_to_lower(author_name) %>% rm_accent(),
+         author_name_extrem = str_detect(author_name_clean, extrem_words)) |>
+  View()
+
+# ------
+
 url_google_sheet <- 'https://docs.google.com/spreadsheets/d/1z3oNAxrIYHZkjwVirRxLKiGMQAyxGE8cAemfXvusUUY/edit#gid=462148650'
 
 users_perfis_direita <- googlesheets4::read_sheet(url_google_sheet, 'perfis_bolsonaristas')
