94f580165726afab2d8a527153ad4a18389e2f13
Beatriz Milz <milz.bea@gmail.com>
2022-12-09 15:11:28 -0300

    busca de seguidores

--- 08-buscar-quem-segue.R
+++ 08-buscar-quem-segue.R
@@ -1,19 +1,11 @@
 library(readr)
-
+library(tidyverse)
 
 base_categorizada_completa <- read_rds("data/base_categorizada_completa.rds")
 
 lista_buscar <- base_categorizada_completa |>
   filter(orientacao_categoria != 'outros', !cod_tweets_magalu %in% c("98", "99")) %>% 
   dplyr::distinct(author_id, author_username, .keep_all = FALSE)
-
-
-# url_google_sheet <- 'https://docs.google.com/spreadsheets/d/1z3oNAxrIYHZkjwVirRxLKiGMQAyxGE8cAemfXvusUUY/edit#gid=462148650'
-
-# base_users <- googlesheets4::read_sheet(url_google_sheet, 'tweets_favoraveis')
-
-# lista_buscar <- base_users |> dplyr::distinct(id, .keep_all = TRUE)
-
 
 users_baixados <- list.files("data/busca_seguidores/") |> 
   stringr::str_remove(".rds")
