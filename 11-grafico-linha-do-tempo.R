88535d0b87c2ec8c3c373faddef54f813338874f
Beatriz Milz <milz.bea@gmail.com>
2022-12-15 21:26:44 -0300

    atualizacao linha do tempo

--- 11-grafico-linha-do-tempo.R
+++ 11-grafico-linha-do-tempo.R
@@ -18,12 +18,8 @@ linha_do_tempo <- googlesheets4::read_sheet("https://docs.google.com/spreadsheet
 
 
 linha_do_tempo_alterada <- linha_do_tempo |> 
-  tidyr::separate(evento, sep = ": ", into = c("evento", "desc_evento")) |> 
-  dplyr::mutate(data = as.Date(data),
-              #  evento = stringr::str_remove(evento, "Evento ")
-              )
-
-
+  tidyr::separate(evento, sep = ": ", into = c("evento", "desc_evento"), remove = FALSE) |> 
+  dplyr::mutate(data = as.Date(data))
 
 
 # Gráfico de tweets ao longo do tempo -------------------------------------
@@ -33,24 +29,41 @@ linha_do_tempo_alterada <- linha_do_tempo |>
 tweets_por_data <- base_completa %>% 
   mutate(data = as.Date(created_at)) %>%  # ajustando a data
   dplyr::left_join(linha_do_tempo_alterada) |> 
-  count(data, evento, desc_evento) # contando quantas linhas existe para cada data
+  count(data, evento, desc_evento, plot) # contando quantas linhas existe para cada data
 
 
-tweets_por_data_com_evento <- tidyr::drop_na(tweets_por_data, evento)
+tweets_por_data_com_evento <- tidyr::drop_na(tweets_por_data, evento) |> 
+  dplyr::mutate(legenda = dplyr::case_when(
+    plot == "SIM" ~ stringr::str_wrap(paste0(
+      evento, ": ", desc_evento
+    ), 20),
+    TRUE ~ evento
+  ))
+
+
 
 # fazendo o gráfico de data através do pacote esquisse
 grafico_linha_do_tempo <- ggplot(tweets_por_data) +
   aes(x = data, y = n) +
   geom_line(size = 0.5, colour = "#112446") +
   geom_point(data = tweets_por_data_com_evento, show.legend = FALSE) + 
- ggrepel::geom_label_repel(data = tweets_por_data_com_evento, aes(label = evento), size = 2, show.legend = FALSE, nudge_y = 6000, nudge_x = 2, segment.color = "gray") + 
+  ggrepel::geom_text_repel(
+    data = tweets_por_data_com_evento,
+    aes(label = legenda, color = plot),
+    size = 3,
+    show.legend = FALSE,
+    nudge_y = 5000,
+    nudge_x = 1.5,
+    segment.color = "gray"
+  ) +
   labs(
     caption = "\n Fonte: Dados extraídos da API do Twitter usando o pacote academictwitteR",
     y = "Quantidade de Tweets",
-    x = "Mês/Ano"
+    x = "Data"
   ) + 
   scale_x_date(date_labels = "%d/%m", limits = c(min(tweets_por_data$data), max(tweets_por_data$data)), date_breaks = "3 day") +
-  theme_light()
+  theme_light() +
+  scale_color_manual(values = c("gray50", "black")) 
 
 grafico_linha_do_tempo
 
