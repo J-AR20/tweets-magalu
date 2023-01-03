2df57db197949340dfddc6820852580cf1121904
Beatriz Milz <milz.bea@gmail.com>
2023-01-03 10:50:49 -0300

    move arquivo

--- scripts/10-grafico-linha-do-tempo.R
+++ scripts/10-grafico-linha-do-tempo.R
@@ -10,15 +10,24 @@ library(ggplot2)
 library(esquisse)
 
 # lendo a base completa
-base_completa <- read_rds('data/base_tweets_completa.rds') %>% 
+base_completa <- read_rds('dados_brutos/04-unir-bases/base_tweets_completa.rds') %>%
   filter(!author_username %in% c('magalu', 'luizatrajano'))
 # removi acima os tweets obviamente partidários da magalu e luizatrajano
 
-linha_do_tempo <- googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1z3oNAxrIYHZkjwVirRxLKiGMQAyxGE8cAemfXvusUUY/edit#gid=1890916712", "linha_do_tempo")
+linha_do_tempo <-
+  googlesheets4::read_sheet(
+    "https://docs.google.com/spreadsheets/d/1z3oNAxrIYHZkjwVirRxLKiGMQAyxGE8cAemfXvusUUY/edit#gid=1890916712",
+    "linha_do_tempo"
+  )
 
 
-linha_do_tempo_alterada <- linha_do_tempo |> 
-  tidyr::separate(evento, sep = ": ", into = c("evento", "desc_evento"), remove = FALSE) |> 
+linha_do_tempo_alterada <- linha_do_tempo |>
+  tidyr::separate(
+    evento,
+    sep = ": ",
+    into = c("evento", "desc_evento"),
+    remove = FALSE
+  ) |>
   dplyr::mutate(data = as.Date(data))
 
 
@@ -26,19 +35,18 @@ linha_do_tempo_alterada <- linha_do_tempo |>
 
 
 # preparação da base
-tweets_por_data <- base_completa %>% 
+tweets_por_data <- base_completa %>%
   mutate(data = as.Date(created_at)) %>%  # ajustando a data
-  dplyr::left_join(linha_do_tempo_alterada) |> 
+  dplyr::left_join(linha_do_tempo_alterada) |>
   count(data, evento, desc_evento, plot) # contando quantas linhas existe para cada data
 
 
-tweets_por_data_com_evento <- tidyr::drop_na(tweets_por_data, evento) |> 
-  dplyr::mutate(legenda = dplyr::case_when(
-    plot == "SIM" ~ stringr::str_wrap(paste0(
-      evento, ": ", desc_evento
-    ), 20),
-    TRUE ~ evento
-  ))
+tweets_por_data_com_evento <-
+  tidyr::drop_na(tweets_por_data, evento) |>
+  dplyr::mutate(legenda = dplyr::case_when(plot == "SIM" ~ stringr::str_wrap(paste0(
+    evento, ": ", desc_evento
+  ), 20),
+  TRUE ~ evento))
 
 
 
@@ -46,7 +54,7 @@ tweets_por_data_com_evento <- tidyr::drop_na(tweets_por_data, evento) |>
 grafico_linha_do_tempo <- ggplot(tweets_por_data) +
   aes(x = data, y = n) +
   geom_line(size = 0.5, colour = "#112446") +
-  geom_point(data = tweets_por_data_com_evento, show.legend = FALSE) + 
+  geom_point(data = tweets_por_data_com_evento, show.legend = FALSE) +
   ggrepel::geom_text_repel(
     data = tweets_por_data_com_evento,
     aes(label = legenda, color = plot),
@@ -56,14 +64,16 @@ grafico_linha_do_tempo <- ggplot(tweets_por_data) +
     nudge_x = 1.5,
     segment.color = "gray"
   ) +
-  labs(
-  #  caption = "\n Fonte: Dados extraídos da API do Twitter usando o pacote academictwitteR",
+  labs(#  caption = "\n Fonte: Dados extraídos da API do Twitter usando o pacote academictwitteR",
     y = "Quantidade de Tweets",
-    x = "Data"
-  ) + 
-  scale_x_date(date_labels = "%d/%m", limits = c(min(tweets_por_data$data), max(tweets_por_data$data)), date_breaks = "3 day") +
+    x = "Data") +
+  scale_x_date(
+    date_labels = "%d/%m",
+    limits = c(min(tweets_por_data$data), max(tweets_por_data$data)),
+    date_breaks = "3 day"
+  ) +
   theme_light() +
-  scale_color_manual(values = c("gray50", "black")) 
+  scale_color_manual(values = c("gray50", "black"))
 
 grafico_linha_do_tempo
 
@@ -76,13 +86,15 @@ ggsave(filename = "graficos/grafico_linha_do_tempo.png", plot = grafico_linha_do
 grafico_simples_por_data <- ggplot(tweets_por_data) +
   aes(x = data, y = n) +
   geom_line(size = 0.5, colour = "#112446") +
-  labs(
-    #  caption = "\n Fonte: Dados extraídos da API do Twitter usando o pacote academictwitteR",
+  labs(#  caption = "\n Fonte: Dados extraídos da API do Twitter usando o pacote academictwitteR",
     y = "Quantidade de Tweets",
-    x = "Data"
-  ) + 
-  scale_x_date(date_labels = "%d/%m", limits = c(min(tweets_por_data$data), max(tweets_por_data$data)), date_breaks = "3 day") +
-  theme_light() 
+    x = "Data") +
+  scale_x_date(
+    date_labels = "%d/%m",
+    limits = c(min(tweets_por_data$data), max(tweets_por_data$data)),
+    date_breaks = "3 day"
+  ) +
+  theme_light()
 
 grafico_simples_por_data
 
